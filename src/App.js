1532116fc4099eaf492d23343f487e4366dfa225
mohsen.rasti <mohsen.rasti@yahoo.com>
2021-10-12 23:52:06 +0330

    component in component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Header from './Component/Header';
 
+import ApprovalCard from './Component/ApprovalCard';
 function App(props) {
   return (
-<>
-<Header author="Ali Rahmanoof" Time="yesterday at 4:45"/>
-<Header author="mohammad saaid azad" Time="today at 1:66"/>
-<Header />
-<button className="ui primary button ">send message</button>
-</>
+    <div className="ui container comments">
+
+      <ApprovalCard>
+        <Header author="Ali Rahmanoof" Time="yesterday at 4:45" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Header author="mohammad saaid azad" Time="today at 1:66" />
+
+
+      </ApprovalCard>
+      <ApprovalCard>
+
+        <Header />
+
+      </ApprovalCard>
+    </div>
+
   );
 }
 
